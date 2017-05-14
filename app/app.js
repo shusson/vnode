8c239d9ff66ff2723e8ac42b41ea2e48af14c8da
Shane Husson <shane.a.husson@gmail.com>
2017-05-14 20:49:43 +1000

    connect to mysql

--- app/app.js
+++ app/app.js
@@ -2,43 +2,44 @@ var express = require('express')
   , cors = require('cors')
   , app = express();
 
+
+let mysql = require('mysql')
+let connection = mysql.createConnection({
+    host     : 'sql',
+    user     : 'root',
+    password : 'a',
+    database : 'v'
+});
+
+connection.connect();
+
+
 app.use(cors());
 app.options('*', cors());
 
 function mockResponse (re,res) {
-  var variants = Array(2000).fill({});
-
-  variants = variants.map(function (e, i) {
-      return {
-          name: 'fred',
-          dbSNP: '',
-          chromosome: 2,
-          start: i,
-          end: i,
-          reference: 'C',
-          alternate: 'G',
-          type: 'INDEL',
-          variantStats: [
-              {
-                  maf: 0.5,
-                  altAlleleFreq: 0.5,
-                  genotypesCount: {
-                    '1/1': 0,
-                    '0/1': 0,
-                    './.':1
-                  }
-              }
-          ],
-      }
-  })
-  var response = {
-      success: true,
-      variants: variants,
-      total: [1]
-  }
-  var waitTill = new Date(new Date().getTime() + 1000);
-  while(waitTill > new Date()){}
-  res.json(response);
+  let c = re.query.chromosome;
+  let start = re.query.positionStart;
+  let end = re.query.positionEnd;
+  let lim = re.query.limit;
+  let skip = re.query.skip ? re.query.skip : 0;
+  let count = "SELECT COUNT(*) AS size FROM vs where chromosome='" + c + "' and start<=" + end + " and start>=" + start;
+  connection.query(count, function (err, count, _) {
+    if (err) throw err
+    let total = count.length > 0 ? count[0].size : 0;
+    if (total > 0) {
+      let query = "SELECT * FROM vs where chromosome='" + c + "' and start<=" + end + " and start>=" + start + " LIMIT " + skip + "," + lim;
+      connection.query(query, function (err, rows, _) {
+        if (err) throw err
+        let response = {
+            success: true,
+            variants: rows,
+            total: [total]
+        }
+        res.json(response);
+      });
+    }
+  });
 }
 
 app.get('*', mockResponse);
